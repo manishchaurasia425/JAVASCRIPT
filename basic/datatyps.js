a30b45af20e9260cb33eb0967ce24ce16cc9eae2
manish kumar chaurasia <manishchaurasia821307@gmail.com>
2024-07-22 12:25:26 +0530

    first commit

---
+++ basic/datatyps.js
@@ -0,0 +1,4 @@
+"use strict";// treat all js code as nwr version
+let isLoggedIn=1;
+let xY=Boolean(isLoggedIn);
+console.log(typeof  xY);
---
+++ basic/x1.js
@@ -0,0 +1,18 @@
+const accountId=144558;
+let accountEmail="manishchaurasia17188gmail.com";
+var accountPass="@Manish$425";
+accountCity="sasaram";
+
+
+// accountId=2; // not allowd to changes
+console.log(accountId);
+accountEmail="hc3";
+console.log(accountEmail);
+accountPass="425";
+console.log(accountPass);
+accountCity="banglore";
+console.log(accountCity);
+
+
+// method2 for printings all variables
+console.table([accountId,accountEmail,accountPass,accountCity]);
